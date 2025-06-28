ae2a926e3c2bdacfb6abea013aad24067bf941bd
Huiheng Li <ahaxlztw@163.com>
2025-06-28 15:05:59 +0800

    created start page and about page

---
+++ ui/Menu/start_menu.gd
@@ -0,0 +1,24 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_start_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://ui/game_ui.tscn") # 暂时还没写好能接入的
+
+
+func _on_about_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://ui/Menu/about_page.tscn")
+
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
