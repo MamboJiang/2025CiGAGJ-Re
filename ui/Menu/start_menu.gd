557fb101b01a57cec2b555d0ae8f66c2b58ac39a
MamboJiang <mambojiang@icloud.com>
2025-06-29 13:47:33 +0800

    Auto stash before merge of "main" and "origin/main"

--- ui/Menu/start_menu.gd
+++ ui/Menu/start_menu.gd
@@ -9,11 +9,14 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if Input.is_action_pressed("p1change") or Input.is_action_pressed("p2change"):
+		Globals.go_to_world("res://bg_map/bg_map.tscn")
 	pass
 
 
 func _on_start_button_pressed() -> void:
 	Globals.go_to_world("res://bg_map/bg_map.tscn")
+
 
 
 func _on_about_button_pressed() -> void:
