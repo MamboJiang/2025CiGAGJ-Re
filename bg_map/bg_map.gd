171fef264a6006f89b312d40ed4750b587e32f43
MamboJiang <mambojiang@icloud.com>
2025-06-29 13:51:32 +0800

    BGM

--- bg_map/bg_map.gd
+++ bg_map/bg_map.gd
@@ -71,31 +71,34 @@ func _input(event):
 	if Input.is_action_just_pressed("p1change"):
 		if not p1_change_timer.is_stopped():
 			return
-		Globals.play_sfx(preload("res://asset/sound/knife.wav"))
 		p1_change_timer.start()
 		match (p1selected):
 			1:
 				if p1hero.get_collider()!=null && not p1hero.get_collider() is TileMap && p1hero.get_collider().side==1:
 					p1selected=p1hero.get_collider().number
 					p1_object=p1hero.get_collider()
+					Globals.play_sfx(preload("res://asset/sound/knife.wav"))
 			2:
 				if p1knife.get_collider()!=null && not p1knife.get_collider() is TileMap && p1knife.get_collider().side==1:
 					p1selected=p1knife.get_collider().number
 					p1_object=p1knife.get_collider()
+					Globals.play_sfx(preload("res://asset/sound/knife.wav"))
 			3:
 				if p1pan.get_collider()!=null && not p1pan.get_collider() is TileMap && p1pan.get_collider().side==1:
 					p1selected=p1pan.get_collider().number
 					p1_object=p1pan.get_collider()
+					Globals.play_sfx(preload("res://asset/sound/knife.wav"))
 			4:
 				if p1milk.get_collider()!=null && not p1milk.get_collider() is TileMap && p1milk.get_collider().side==1:
 					p1selected=p1milk.get_collider().number
 					p1_object=p1milk.get_collider()
+					Globals.play_sfx(preload("res://asset/sound/knife.wav"))
 		if p1selected!=1:
 			p1hero.disappear()
 		print("P1 切换到: ", p1selected)
 	if Input.is_action_just_pressed("p1_quit"):
-		Globals.play_sfx(preload("res://asset/sound/knife.wav"))
 		if p1selected!=1:
+			Globals.play_sfx(preload("res://asset/sound/knife.wav"))
 			p1hero.appear(p1_object.position)
 			p1selected=1
 	
@@ -103,31 +106,35 @@ func _input(event):
 	if Input.is_action_just_pressed("p2change"):
 		if not p2_change_timer.is_stopped():
 			return
-		Globals.play_sfx(preload("res://asset/sound/knife.wav"))
 		p2_change_timer.start()
 		match (p2selected):
 			1:
 				if p2hero.get_collider()!=null && not p2hero.get_collider() is TileMap && p2hero.get_collider().side==2:
 					p2selected=p2hero.get_collider().number
 					p2_object=p2hero.get_collider()
+					Globals.play_sfx(preload("res://asset/sound/knife.wav"))
 			2:
 				if p2knife.get_collider()!=null && not p2knife.get_collider() is TileMap && p2knife.get_collider().side==2:
 					p2selected=p2knife.get_collider().number
 					p2_object=p2knife.get_collider()
+					Globals.play_sfx(preload("res://asset/sound/knife.wav"))
 			3:
 				if p2pan.get_collider()!=null && not p2pan.get_collider() is TileMap && p2pan.get_collider().side==2:
 					p2selected=p2pan.get_collider().number
 					p2_object=p2pan.get_collider()
+					Globals.play_sfx(preload("res://asset/sound/knife.wav"))
 			4:
 				if p2milk.get_collider()!=null && not p2milk.get_collider() is TileMap && p2milk.get_collider().side==2:
 					p2selected=p2milk.get_collider().number
 					p2_object=p2milk.get_collider()
+					Globals.play_sfx(preload("res://asset/sound/knife.wav"))
 		if p2selected!=1:
 			p2hero.disappear()
+
 		print("P2 切换到: ", p2selected)
 	if Input.is_action_just_pressed("p2_quit"):
-		Globals.play_sfx(preload("res://asset/sound/knife.wav"))
 		if p2selected!=1:
+			Globals.play_sfx(preload("res://asset/sound/knife.wav"))
 			p2hero.appear(p2_object.position)
 			p2selected=1
 	
